562ff03ec59625e958e4d1191e5f3a50cb9b479c
Vedansh Vijaywargiya <60468275+vvHacker007@users.noreply.github.com>
2021-03-30 14:44:07 +0530

    Frontend changes

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template,session,flash,url_for,request,redirect
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/home.js
@@ -0,0 +1,6 @@
+document.getElementById("icon_stats").onclick = function() { myFunction() };
+
+function myFunction() {
+    document.getElementById("icon_stats").color = "#1c8d76";
+
+}
